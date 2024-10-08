219ac9feb2e7a333338b77e88b7747951706a771
armedturret <rjwhitenh@outlook.com>
2024-10-08 09:00:17 -0400

    Created the initial python file

---
+++ sigmadb.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+
+import sys
+
+def main():
+    """
+    The entry point for the program
+
+    :return: 0 on success
+    """
+
+    print("Hi :3")
+    
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
