fe19f9ecb1cb12afaeb5ba786aa4b981224bb1b1
dariu <djn6814@rit.edu>
2024-10-28 11:30:32 -0400

    finished implementing create_collection, moving onto modify_collection

--- user_funcs.py
+++ user_funcs.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 import input_utils
 import hashlib
+import movie_funcs
 
 MAX_INPUT_LEN = 255
 
@@ -117,10 +118,81 @@ def login(conn) -> tuple[str, int]:
 
             print("Username or password incorrect!")
 
-def create_collection(conn):
-    user_collection = list()
-    loop_state = True
-    while loop_state:
-        
+def create_collection(conn, user_ID) -> None:
+    """
+    creates a new collection for the user and puts the new collection
+    in the movie collection table
 
-        return [""]
+    modifying the collection and listing the collection are not in this method
+
+    :param conn: The connection to the database to execute SQL statements
+    :param user_ID: The ID of the user for their movie collection
+    :return: Nothing
+    """
+    collection_ID = 0
+    collection_Name = ""
+    loop_state = True
+    with conn.cursor() as curs:
+        #Query that will be inserted into the MovieCollection table
+        query = """
+        INSERT INTO moviecollection (collectionid, name, madeby)
+        VALUES(collection_ID, collcection_Name, user_ID)
+        """
+        #A query that will insert the movie_ID the user selected into the incollection table with associated collection_ID
+        query_two = """
+        INSERT INTO incollection (movieid, collectionid)
+        VALUES(movie_ID, collection_ID)
+        """
+
+        #Query that gets the last collection ID in the moviecollection table
+        collection_ID_query = """
+        Select LAST(collectionid) FROM moviecollection
+        """
+
+        #Loop that prompts user for information
+        print("\ncreate_collection function running...")
+        decision = int(input_utils.get_input_matching("""\nTo start creating a new collection, would you like to
+                    \nCreate a new collection that will be filled with movies (type 1)
+                    \nCreate an empty collection (Type 0): """), regex="[01]")
+        if decision == 0:
+            curs.execute(collection_ID_query, ())
+            collection_ID_results = int(curs.fetchone()[0]) + 1
+            collection_ID = str(collection_ID_results)
+            collection_Name = input("What would you like to name your new collection?\n")
+            curs.execute(query, (collection_ID, collection_Name, user_ID))
+        elif decision == 1:
+            curs.execute(collection_ID_query, ())
+            collection_ID_results = int(curs.fetchone()[0])
+            collection_ID_results += 1
+            collection_ID = str(collection_ID_results)
+            collection_Name = input("What would you like to name your new collection?\n")
+            curs.execute(query, (collection_ID, collection_Name, user_ID))
+            movie_id = 0
+            while loop_state:
+                #adding movies that are in the database to a collection when making a new collection
+                break_loop = 0
+                print("what movie would you like to add?")
+                movie_id = movie_funcs.browse_movies(conn)
+                if movie_id == -1:
+                    print("Was not able to get movieID to add to collectio.\nWill Exit...")
+                    loop_state = False
+                    break
+                else:
+                    # Not going to work since you are making a single new collection with movies connected to that collection
+                    # Movies have to be looped to connect the collection to that specific movie
+                    movie_ID = str(movie_id)
+                    curs.execute(query_two, (movie_ID, collection_ID))
+            else:
+                print("Not an acceptable answer for decision! Ending function")
+                return
+    return
+"""
+
+"""
+def modify_collection(conn) -> None:
+    pass
+"""
+
+"""
+def display_collection(conn) -> None:
+    pass
